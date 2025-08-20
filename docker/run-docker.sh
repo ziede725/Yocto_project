c28600708dcea66cad6363b6944696dd37dca2db
zied <ziede.ben.yahya@gmail.com>
2025-08-20 17:22:47 +0100

    fix(docker run script):made the script run the same container at docker run instead of creating

--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-IMAGE_NAME="my-yocto"
-YOCTO_DIR="$(pwd)"        
+IMAGE_NAME="my-yocto.2"
+YOCTO_DIR="/home/zied/techleefYocto_Project/Yocto_project"        
 KAS_CONFIG="project.yml"  
-
+YOCTO_DL_DIR="/home/zied/yocto/build/downloads"
+YOCTO_CACHE_DIR="/home/zied/yocto/build/sstate-cache" 
 # Check if Docker image exists
 if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
     echo "Docker image '$IMAGE_NAME' not found. Building..."
@@ -14,13 +15,24 @@ else
     echo "Docker image '$IMAGE_NAME' already exists."
 fi
 
+CONTAINER_NAME="competent_kalam"
+
+# If container exists, start it
+if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Starting existing container..."
+    docker start -ai $CONTAINER_NAME
+else
 # Run container
 docker run -it \
     -v "$YOCTO_DIR":/work \
+    -v "$YOCTO_DL_DIR":/work/build/downloads \
+    -v "$YOCTO_CACHE_DIR":/work/build/sstate-cache \
     -w /work \
     -u yocto \
     $IMAGE_NAME /bin/bash
+fi
 
+    
 #    bash -c '
 # Install kas for yocto user if not already installed
 #export PATH="$HOME/.local/bin:$PATH"
